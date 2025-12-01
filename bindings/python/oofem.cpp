f0cf98c8e4544f646a1fce94ea02e6fdf89d242f
gereonwildermann <gereon.wildermann@t-online.de>
2025-12-01 09:25:46 +0100

    Add giveCrossSectionReduction method to Rebar3d Python bindings

--- bindings/python/oofem.cpp
+++ bindings/python/oofem.cpp
@@ -1267,6 +1267,7 @@ PYBIND11_MODULE(oofempy, m) {
 
     py::class_<oofem::Rebar3d, oofem::Element, PyElement<oofem::Rebar3d>>(m, "Rebar3d")
         .def("computeMassCorroded", &oofem::Rebar3d::computeMassCorroded)
+        .def("giveCrossSectionReduction", &oofem::Rebar3d::giveCrossSectionReduction)
     ;
 
     py::class_<oofem::LSpace, oofem::Element, PyElement<oofem::LSpace>>(m, "LSpace")
